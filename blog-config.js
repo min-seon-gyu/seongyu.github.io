d71e9609e22fb8ff912e157a5d4fb61d51db254f
Seongyu <gcael@naver.com>
2024-11-11 23:50:19 +0900

    config 설정

---
+++ blog-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: "seongyumin.blog",
+  description: "학습한 내용을 기록하는 블로그입니다.",
+  author: "seongyumin",
+  siteUrl: "https://min-seon-gyu.github.io",
+  links: {
+    github: "https://github.com/min-seon-gyu",
+  },
+  useAbout: false,
+  utterances: {
+    repo: 'min-seon-gyu/min-seon-gyu.github.io',
+    type: "pathname"
+  },
+  giscus: {
+    repo: "min-seon-gyu/min-seon-gyu.github.io",
+    repoId: "R_kgDONNGVDw",
+    category: "Comments",
+    categoryId: "DIC_kwDONNGVD84CkK38",
+    mapping: "pathname",
+    strict: "1",
+    reactionsEnabled: "1",
+    inputPosition: "bottom",
+    lang: "ko",
+  },
+}
